fa892fb6c3577d49bbcb41dc62d05458f383e040
William Dibbern <gotdibbs@outlook.com>
2016-01-19 10:19:37 -0600

    Update show fields to expand all sections

--- src/bookmarklets/show-all-fields.js
+++ src/bookmarklets/show-all-fields.js
@@ -3,8 +3,13 @@ formContext.Xrm.Page.ui.controls.forEach(function(c, i){
 });
 
 formContext.Xrm.Page.ui.tabs.forEach(function(c, i){
+    // Show the tab
     c.setVisible(true);
     
+    // Expand the tab
+    c.setDisplayState('expanded');
+
+    // Show all the sections contained in the tab
     c.sections.forEach(function(sc, si){
         sc.setVisible(true);
     });
