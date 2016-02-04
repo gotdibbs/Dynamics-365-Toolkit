701d990046878212f1993458b272eca19d24a4be
William Dibbern <gotdibbs@outlook.com>
2016-02-03 19:02:40 -0600

    Bookmarklet UX Improvements

--- src/bookmarklets/outro.js
+++ src/bookmarklets/outro.js
@@ -1,6 +1,6 @@
 }(function (global) {
     if (!global.APPLICATION_VERSION) {
-        return alert('Could not determine the current version of CRM.');
+        return alert('Could not determine the current version of CRM. Please ensure you\'re viewing a record in Dynamics CRM.');
     }
     if (global.APPLICATION_VERSION === '5.0') {
         return window.top.frames[0];
@@ -12,13 +12,18 @@
             return $iframe[0].contentWindow;
         }
         else {
-            return alert('[CRM 2013/2015/2016] Could not locate the entity form.');
+            return alert('[CRM 2013/2015/2016] Could not locate the entity form. Please ensure you\'re viewing a record in Dynamics CRM.');
         }
     }
     else if (global.APPLICATION_VERSION) {
-        return alert('Unsupported CRM Version Detected: ' + global.APPLICATION_VERSION);
+        return alert([
+            'Unsupported CRM Version Detected: ', global.APPLICATION_VERSION, '.',
+            ' Please check https://gotdibbs.com/crm/help/ for an updated version of this bookmarklet',
+            ' or email webmaster@gotdibbs.net and let us know that this version of CRM',
+            ' isn\'t working.'
+        ].join(''));
     }
     else {
-        return alert('Unable to detect current CRM Version.');
+        return alert('Unable to detect current CRM Version. Please ensure you\'re viewing a record in Dynamics CRM.');
     }
 }(window)));
