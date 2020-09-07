71a81208613d265b399095d561f4c8f9f848e6ad
William Dibbern <gotdibbs@outlook.com>
2020-09-07 13:28:56 -0500

    Add tests to CI - Closes #29

--- chromeExtension/tests/integration/wdio.conf.js
+++ chromeExtension/tests/integration/wdio.conf.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const capabilities = require('./capabilities.json');
 const AuthenticationManager = require('./utilities/AuthenticationManager');
 const ScenarioHelper = require('./utilities/ScenarioHelper');
 
@@ -69,28 +70,7 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [
-        {
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
-            maxInstances: 1,
-            //
-            browserName: 'chrome',
-            browserVersion: 'beta'
-            // If outputDir is provided WebdriverIO can capture driver session logs
-            // it is possible to configure which logTypes to include/exclude.
-            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-            // excludeDriverLogs: ['bugreport', 'server'],
-        },
-        // {
-        //     maxInstances: 1,
-        //     browserName: 'Edge',
-        //     browserVersion: 'latest',
-        //     // Empty options object seems to be needed for some reason
-        //     'bstack:options' : {}
-        // }
-    ],
+    capabilities: capabilities,
     //
     // ===================
     // Test Configurations
